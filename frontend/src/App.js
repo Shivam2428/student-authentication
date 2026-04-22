20ed096ae39f67a47e41c3fcce2e143c8b8c5339
Shivam Rai <rai268359@gmail.com>
2026-04-23 02:20:03 +0530

    fix path issue final

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./Login";
-import Register from "./Register";
-import Dashboard from "./Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
